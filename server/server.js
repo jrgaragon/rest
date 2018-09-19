decc5ac377fd421235f1366c92977ae94bd74c31
Jorge Aragon <43160434+jrgaragon@users.noreply.github.com>
2018-09-18 21:59:21 -0700

    added POST and PUT

--- server/server.js
+++ server/server.js
@@ -1,40 +1,23 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get user')
-});
+app.use(require('./routes/users'));
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'Nombre field is required.'
-        });
+mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true }, (err) => {
+    if (err) {
+        throw err;
     } else {
-        res.json({ persona: body });
+        console.log('DB Connected');
     }
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete user')
 });
 
 app.listen(process.env.PORT, () => {
